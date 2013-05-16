0170643780a1cf33974bb972a097df48d1f73bd1
Jan-Marten de Boer <github@johmanx.com>
2013-05-16 19:52:38 +0200

    Adding Ubuntu 13.04

---
+++ ubuntu-13.04/hardware/AMD/graphics/watermarkfix.sh
@@ -0,0 +1,11 @@
+#bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+  echo "$0 must be run as root"
+  exit 1
+fi
+
+DRIVER=/usr/lib/fglrx/xorg/modules/drivers/fglrx_drv.so
+for x in $(objdump -d $DRIVER|awk '/call/&&/EnableLogo/{print "\\x"$2"\\x"$3"\\x"$4"\\x"$5"\\x"$6}'); do
+sed -i "s/$x/\x90\x90\x90\x90\x90/g" $DRIVER
+done
