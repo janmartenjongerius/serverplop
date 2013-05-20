a005ec92f344e651c6b8b67edad10ee1c882ab86
Jan-Marten de Boer <github@johmanx.com>
2013-05-20 11:16:11 +0200

    Fix AMD fix

--- ubuntu-13.04/hardware/AMD/graphics/watermarkfix.sh
+++ ubuntu-13.04/hardware/AMD/graphics/watermarkfix.sh
@@ -1,4 +1,4 @@
-#bin/bash
+#!/bin/bash
 
 # Exit the script when a command returns a non true value.
 set -e
@@ -14,3 +14,5 @@ DRIVER=/usr/lib/fglrx/xorg/modules/drivers/fglrx_drv.so
 for x in $(objdump -d $DRIVER|awk '/call/&&/EnableLogo/{print "\\x"$2"\\x"$3"\\x"$4"\\x"$5"\\x"$6}'); do
 sed -i "s/$x/\x90\x90\x90\x90\x90/g" $DRIVER
 done
+
+echo "Logout or restart your desktop manager for the changes to take effect"
